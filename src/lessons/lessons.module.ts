322dc9aebbfd7c387a234f37adbd219e4b4992b2
karlovcihlar <karlo-valentin.cihlar@fer.hr>
2022-03-02 23:48:13 +0100

    fixed import error

--- src/lessons/lessons.module.ts
+++ src/lessons/lessons.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { LessonsService } from './lessons.service';
 import { LessonsController } from './lessons.controller';
+import { LessonsRepository } from './lessons.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([LessonsRepository])],
   controllers: [LessonsController],
   providers: [LessonsService]
 })
