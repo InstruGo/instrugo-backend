e520e10aecbf7905450728440ff2be8b0b547c0f
Ivan Skorupan <ivan.skorupan@fer.hr>
2022-04-16 02:04:15 +0200

    add credentials property to cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['http://localhost:3000', 'http://localhost:3001'],
+    credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
