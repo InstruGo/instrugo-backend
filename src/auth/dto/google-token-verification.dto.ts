4c60199738bba225f4bc0606dffae0ed5d6fcf4c
Ivan Skorupan <ivan.skorupan@fer.hr>
2022-05-07 19:38:53 +0200

    add api property decorator to google token dto

--- src/auth/dto/google-token-verification.dto.ts
+++ src/auth/dto/google-token-verification.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class GoogleTokenVerificationDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   token: string;
 }
